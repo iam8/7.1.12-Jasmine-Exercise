013a6df989d87a8b166d2dc3e57ac981e0a874b5
alexm <alex-m@outlook.com>
2022-10-05 16:40:18 -0700

    Added tests for invalid input nums

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -3,12 +3,25 @@
 // 7.1.12: Jasmine Testing Exercise
 
 describe("Tests for calculateMonthlyPayment()", function() {
-    it('should calculate the monthly rate correctly', function() {
-        // ...
+    it("Should throw an error for invalid inputs", function() {
+        // Negative loan amount, yearly rate, or num of years
+        expect(() => calculateMonthlyPayment({amount: -10, rate: 1, years: 1})).toThrowError("Input amounts cannot be negative.");
+        expect(() => calculateMonthlyPayment({amount: 1, rate: -10, years: 1})).toThrowError("Input amounts cannot be negative.");
+        expect(() => calculateMonthlyPayment({amount: 1, rate: 1, years: -10})).toThrowError("Input amounts cannot be negative.");
+
+        // Value of 0 for yearly rate or num years
+        expect(() => calculateMonthlyPayment({amount: 1, rate: 0, years: 1})).toThrowError("Yearly rate and number of years must be greater than 0.");
+        expect(() => calculateMonthlyPayment({amount: 1, rate: 1, years: 0})).toThrowError("Yearly rate and number of years must be greater than 0.");
+    })
+
+    it('Should calculate the monthly rate correctly', function() {
+        // Should return a monthly payment of 0 if the loan amount is 0
+
+        // Should calculate correctly otherwise
     });
     
     
-    it("should return a result with 2 decimal places", function() {
+    it("Should return a result with 2 decimal places", function() {
         // ..
     });
     
