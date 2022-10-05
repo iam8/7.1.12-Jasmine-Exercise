3744d43325ae152df0532522aef27da940791f78
alexm <alex-m@outlook.com>
2022-10-05 16:06:11 -0700

    Added comments to functions; added code to update() and calculateMonthlyPayment()

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,17 +2,19 @@
 // 10/05/22
 // 7.1.12: Jasmine Testing Exercise
 
+// After DOM is loaded, set up initial input values and add a submit event listener to the form
 window.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById("calc-form");
     if (form) {
         setupIntialValues();
         form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        update();
+            e.preventDefault();
+            update();
         });
     }
 });
 
+// Retrieve the text input values (user input) from the DOM
 function getCurrentUIValues() {
     return {
         amount: +(document.getElementById("loan-amount").value),
@@ -30,12 +32,19 @@ function setupIntialValues() {
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+    const ui_values = getCurrentUIValues();
+    return calculateMonthlyPayment(ui_values);
 }
 
 // Given an object of values (a value has amount, years and rate ),
-// calculate the monthly payment.  The output should be a string
-// that always has 2 decimal places.
+// calculate the monthly payment.
+// The output should be a string that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+    const principle = values.amount;
+    const interest_rate = values.rate / 12;
+    const num_payments = values.years * 12;
+
+    return (principle * interest_rate) / (1 - (1 + interest_rate)**(-num_payments));
 }
 
 // Given a string representing the monthly payment value,
