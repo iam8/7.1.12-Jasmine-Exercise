8b8fb63863620e653370695c1b3575ba0027c64d
alexm <alex-m@outlook.com>
2022-10-06 16:44:18 -0700

    Moved new test files (for helpers.js and payments.js) into tip-pool folder; added describe() and some it() sections for helpers.test.js

--- payments.test.js
+++
@@ -1,3 +0,0 @@
-// Ioana A Mititean
-// 10/06/22
-// 7.1.12: Jasmine Testing Exercise
---
+++ tip-pool/helpers.test.js
@@ -0,0 +1,34 @@
+// Ioana A Mititean
+// 10/06/22
+// 7.1.12: Jasmine Testing Exercise
+
+describe("Tests for sumPaymentTotal() (with setup and teardown)", function() {
+
+    // Initialization logic
+
+    
+
+    // Teardown logic
+
+})
+
+describe("Tests for calculateTipPercent()", function() {
+
+    it("Should throw an error for zero or negative tip and bill amounts", function() {
+
+    })
+
+    it("Should calculate the correct tip percent when given valid tip and bill amounts",
+        function() {
+
+        })
+
+})
+
+describe("Tests for appendTd() (with setup and teardown)", function() {
+
+    // Initialization logic
+
+    // Teardown logic
+
+})
