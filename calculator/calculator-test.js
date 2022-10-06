2838b3aa5d42f1007b26747d2dc9d5d0b81705b5
alexm <alex-m@outlook.com>
2022-10-05 17:03:46 -0700

    Broke up long lines of code into multiple (for readability); added expect() for loan amounts of 0; added test for input values of NaN

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -4,20 +4,60 @@
 
 describe("Tests for calculateMonthlyPayment()", function() {
     it("Should throw an error for invalid inputs", function() {
+
         // Negative loan amount, yearly rate, or num of years
-        expect(() => calculateMonthlyPayment({amount: -10, rate: 1, years: 1})).toThrowError("Input amounts cannot be negative.");
-        expect(() => calculateMonthlyPayment({amount: 1, rate: -10, years: 1})).toThrowError("Input amounts cannot be negative.");
-        expect(() => calculateMonthlyPayment({amount: 1, rate: 1, years: -10})).toThrowError("Input amounts cannot be negative.");
+        expect(() => calculateMonthlyPayment({
+            amount: -10,
+            rate: 1,
+            years: 1}))
+            .toThrowError("Input amounts cannot be negative.");
+        expect(() => calculateMonthlyPayment({
+            amount: 1,
+            rate: -10,
+            years: 1}))
+            .toThrowError("Input amounts cannot be negative.");
+        expect(() => calculateMonthlyPayment({
+            amount: 1,
+            rate: 1,
+            years: -10}))
+            .toThrowError("Input amounts cannot be negative.");
 
         // Value of 0 for yearly rate or num years
-        expect(() => calculateMonthlyPayment({amount: 1, rate: 0, years: 1})).toThrowError("Yearly rate and number of years must be greater than 0.");
-        expect(() => calculateMonthlyPayment({amount: 1, rate: 1, years: 0})).toThrowError("Yearly rate and number of years must be greater than 0.");
+        expect(() => calculateMonthlyPayment({
+            amount: 1,
+            rate: 0,
+            years: 1}))
+            .toThrowError("Yearly rate and number of years must be greater than 0.");
+        expect(() => calculateMonthlyPayment({
+            amount: 1,
+            rate: 1,
+            years: 0}))
+            .toThrowError("Yearly rate and number of years must be greater than 0.");
+        
+        // Input values of NaN
+        expect(() => calculateMonthlyPayment({
+            amount: NaN,
+            rate: 1,
+            years: 1}))
+            .toThrowError("Inputs must be numbers.");
+        expect(() => calculateMonthlyPayment({
+            amount: 1,
+            rate: NaN,
+            years: 1}))
+            .toThrowError("Inputs must be numbers.");
+        expect(() => calculateMonthlyPayment({
+            amount: 1,
+            rate: 1,
+            years: NaN}))
+            .toThrowError("Inputs must be numbers.");
     })
 
     it('Should calculate the monthly rate correctly', function() {
-        // Should return a monthly payment of 0 if the loan amount is 0
 
-        // Should calculate correctly otherwise
+        // Should return a monthly payment of 0 if the loan amount is 0
+        expect(calculateMonthlyPayment({amount: 0, rate: 1, years: 1})).toBe(0);
+
+        // Should calculate correctly for other numbers
     });
     
     
