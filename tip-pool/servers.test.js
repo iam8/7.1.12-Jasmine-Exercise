c82523fad36dc8b5e10f21e8417a802c059fbaf5
alexm <alex-m@outlook.com>
2022-10-05 23:13:08 -0700

    Adjusted line intendation

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -3,19 +3,19 @@
 // 7.1.12: Jasmine Testing Exercise
 
 describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+    beforeEach(function () {
+        // initialization logic
+        serverNameInput.value = 'Alice';
+    });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+    it('should add a new server to allServers on submitServerInfo()', function () {
+        submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+        expect(Object.keys(allServers).length).toEqual(1);
+        expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
 
-  afterEach(function() {
-    // teardown logic
-  });
+    afterEach(function() {
+        // teardown logic
+    });
 });
