cbed222aafee83e290cb8e0641665f83c58f9504
alexm <alex-m@outlook.com>
2022-10-05 23:57:06 -0700

    Added a test for submitServerInfo(); added a placeholder for another test for submitServerInfo()

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -9,12 +9,23 @@ describe("Servers test (with setup and tear-down)", function() {
         serverNameInput.value = 'Alice';
     });
 
-    it('should add a new server to allServers on submitServerInfo()', function () {
+    it('Should add a new server to allServers on submitServerInfo()', function() {
         submitServerInfo();
 
         expect(Object.keys(allServers).length).toEqual(1);
         expect(allServers['server' + serverId].serverName).toEqual('Alice');
     });
+
+    it("Should update the table HTML with the new server information", function() {
+        submitServerInfo();
+
+    })
+
+    it("Should reset input value for server name in submitServerInfo()", function() {
+        submitServerInfo()
+
+        expect(serverNameInput.value).toBe("");
+    })
 
     // Teardown logic
     afterEach(function() {
