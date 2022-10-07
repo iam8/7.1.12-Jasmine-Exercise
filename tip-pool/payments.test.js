1f4c6a4d1161add6ea0ac0ffeb4efba81c58f710
alexm <alex-m@outlook.com>
2022-10-06 19:09:04 -0700

    Removed commented out code

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -10,33 +10,6 @@ describe("submitPaymentInfo() tests", function() {
         tipAmtInput.value = 25;
     })
 
-    // it("Should not make any changes if empty bill and tip amounts are entered", function() {
-    //     billAmtInput.value = "";
-    //     tipAmtInput.value = "";
-
-    //     // Get the initial HTML for each table that would be modified in the DOM
-    //     const summaryTbody = document.querySelector("#summaryTable tbody");
-
-    //     const paymentTbodyHtmlInit = paymentTbody.innerHTML;
-    //     const serverTbodyHtmlInit = serverTbody.innerHTML;
-    //     const summaryTbodyHtmlInit = summaryTbody.innerHTML;
-
-    //     submitPaymentInfo();
-
-    //     expect(paymentId).toBe(0);
-    //     expect(allPayments).toEqual({});
-
-    //     // Get the HTML for each table again (after submitPaymentInfo was called)
-    //     const paymentTbodyHtmlFinal = paymentTbody.innerHTML;
-    //     const serverTbodyHtmlFinal = serverTbody.innerHTML;
-    //     const summaryTbodyHtmlFinal = summaryTbody.innerHTML;
-
-    //     // Compare final HTML with initial HTML
-    //     expect(paymentTbodyHtmlFinal).toBe(paymentTbodyHtmlInit);
-    //     expect(serverTbodyHtmlFinal).toBe(serverTbodyHtmlInit);
-    //     expect(summaryTbodyHtmlFinal).toBe(summaryTbodyHtmlInit);
-    // })
-
     it ("Should add a new entry of payment info to allPayments",
         function() {
             submitPaymentInfo();
